937c0d5b5458ea860dccae27bf893c4d6c17c4c3
j0hnpa9e <j0hnpa9e@gmail.com>
2015-06-27 16:26:00 +0100

    First hack for testing out the ideas and coming up with a plan.

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Jesture
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this._paper = new System.Windows.Forms.Panel();
+         this.SuspendLayout();
+         // 
+         // _paper
+         // 
+         this._paper.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this._paper.Location = new System.Drawing.Point(12, 12);
+         this._paper.Name = "_paper";
+         this._paper.Size = new System.Drawing.Size(639, 323);
+         this._paper.TabIndex = 0;
+         this._paper.Paint += new System.Windows.Forms.PaintEventHandler(this.paper_Paint);
+         this._paper.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paper_MouseDown);
+         this._paper.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paper_MouseMove);
+         this._paper.MouseUp += new System.Windows.Forms.MouseEventHandler(this.paper_MouseUp);
+         this._paper.Resize += new System.EventHandler(this._paper_Resize);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(663, 347);
+         this.Controls.Add(this._paper);
+         this.Name = "Form1";
+         this.Text = "Jesture";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Panel _paper;
+   }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Jesture
+{
+   public partial class Form1 : Form
+   {
+      Graphics _g;
+
+      bool _drawing = false;
+      Point? _startPoint = null;
+      Point _endPoint = new Point();
+      Pen _pen = new Pen(Color.Black);
+      List<Tuple<Point, Point>> _lines = new List<Tuple<Point, Point>>();
+      bool _panning = false;
+      Matrix _panTransform = new Matrix();
+
+      DateTime _strokeStartTime = DateTime.Now;
+
+
+      public Form1()
+      {
+         InitializeComponent();
+
+         _g = _paper.CreateGraphics();
+      }
+
+      private void paper_Paint(object sender, PaintEventArgs e)
+      {
+         _g = _paper.CreateGraphics();
+         _g.Transform = _panTransform;
+
+
+         //Make some drawable types: System, Actor, Line
+
+         foreach (var line in _lines)
+         {
+            _g.DrawLine(_pen, line.Item1, line.Item2);
+         }
+      }
+
+      private void paper_MouseDown(object sender, MouseEventArgs e)
+      {
+         if (e.Button == MouseButtons.Left)
+         {
+            _drawing = true;
+            _strokeStartTime = DateTime.Now;
+         }
+         else if (e.Button == MouseButtons.Right)
+         {
+            _panning = true;
+         }
+         _startPoint = e.Location;
+      }
+
+      private void paper_MouseUp(object sender, MouseEventArgs e)
+      {
+         _panning = false;
+
+         _drawing = false;
+         _endPoint = e.Location;
+
+         //if (_startPoint.HasValue)
+         //{
+         //   Point[] location = { _startPoint.Value, _endPoint };
+         //   var transform = _panTransform.Clone();
+         //   transform.Invert();
+         //   transform.TransformPoints(location);
+
+         //   _lines.Add(new Tuple<Point, Point>(location[0], location[1]));
+         //   _startPoint = null;
+         //}
+
+         //_paper.Invalidate();
+      }
+
+      private void paper_MouseMove(object sender, MouseEventArgs e)
+      {
+         _endPoint = e.Location;
+
+         var elapsedMilliSeconds = (DateTime.Now.Ticks - _strokeStartTime.Ticks) / 10000;
+         if (_drawing && elapsedMilliSeconds > 50)
+         {
+            Point[] location = { e.Location };
+            var transform = _panTransform.Clone();
+            transform.Invert();
+            transform.TransformPoints(location);
+            _g.DrawEllipse(_pen, new Rectangle(location[0], new Size(5, 5)));
+            
+            if (_startPoint.HasValue)
+            {
+               Point[] location2 = { _startPoint.Value, _endPoint };
+               var transform2 = _panTransform.Clone();
+               transform2.Invert();
+               transform2.TransformPoints(location2);
+
+               _lines.Add(new Tuple<Point, Point>(location2[0], location2[1]));
+               _startPoint = e.Location;
+            } 
+            _strokeStartTime = DateTime.Now;
+            _paper.Invalidate();
+         }
+         else if (_panning)
+         {
+            _panTransform.Translate(
+               e.Location.X - _startPoint.Value.X,
+               e.Location.Y - _startPoint.Value.Y);
+
+            _startPoint = e.Location;
+            _paper.Invalidate();
+         }
+      }
+
+      private void _paper_Resize(object sender, EventArgs e)
+      {
+      }
+   }
+}
